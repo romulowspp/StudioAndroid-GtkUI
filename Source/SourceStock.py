5082166a6c5e63bda37b2caa1a1f660470cc22c8
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2012-05-30 21:24:58 +0200

    Wed May 30 21:24:58 CEST 2012

---
+++ Source/SourceStock.py
@@ -0,0 +1,4 @@
+Sources = ['GB-AOSP', 'CM7', 'ICS-AOSP', 'AOKP', 'CM9']
+URL = ['repo init -u https://android.googlesource.com/platform/manifest -b android-2.3.7_r1', 'repo init -u git://github.com/CyanogenMod/android.git -b gingerbread ', 'repo init -u https://android.googlesource.com/platform/manifest -b android-4.0.1_r1', 'repo init -u git://github.com/AOKP/platform_manifest.git -b master', 'repo init -u git://github.com/CyanogenMod/android.git -b ics']
+
+
