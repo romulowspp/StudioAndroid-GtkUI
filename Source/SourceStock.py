89aadb8c677d4a3f0bdac2155a1e6caf0fc73617
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2012-06-05 18:01:30 +0200

    Tue Jun  5 18:01:30 CEST 2012

--- Source/SourceStock.py
+++ Source/SourceStock.py
@@ -1,4 +1,10 @@
 Sources = ['GB-AOSP', 'CM7', 'ICS-AOSP', 'AOKP', 'CM9']
 URL = ['repo init -u https://android.googlesource.com/platform/manifest -b android-2.3.7_r1', 'repo init -u git://github.com/CyanogenMod/android.git -b gingerbread ', 'repo init -u https://android.googlesource.com/platform/manifest -b android-4.0.1_r1', 'repo init -u git://github.com/AOKP/platform_manifest.git -b ics', 'repo init -u git://github.com/CyanogenMod/android.git -b ics']
 
+# To add a new source, use this: (w/o the # )
+#
+# Sources.append('Source_name (WITHOUT SPACES!)')
+# URL.append('repo init *** command for that source')
+
+
 
